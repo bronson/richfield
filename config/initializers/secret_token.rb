87892bb08872d1317551fb28015dc869d4e2d29e
Scott Bronson <brons_git@rinspin.com>
2013-01-25 18:36:46 -0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Richfield::Application.config.secret_token = '901bff62346b0d2b0b36fc86a1c24dafe28547a7514e097cbcae187430a86febfefff5eeaac8b33d1f3fb1efbb1b02419a1aceb355fadfa6d9726ea0755297f7'
