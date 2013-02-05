61bb6e2b4a0412298965d4f9f76c812dccc9d5ea
Scott Bronson <brons_git@rinspin.com>
2013-02-05 08:33:39 -0800

    further syntax cleanup to some tests

--- spec/migrator/columns_spec.rb
+++ spec/migrator/columns_spec.rb
@@ -17,222 +17,214 @@ describe Richfield::Migrator do
     table :truly_empty do
     end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :add_column, table: 'truly_empty', name: 'name', type: :string, options: { default: "nope" } }
-      ]}
-    )
+      ]} )
   end
 
   it "handles switching belongs_to ownership" do
-    model(:handlers) {
+    model(:handlers) do
       fields
       belongs_to :dog
-    }
-    model(:dogs) {
+    end
+    model(:dogs) do
       fields
       has_many :handlers
-    }
+    end
 
-    table(:handlers) { |t|
+    table(:handlers) do |t|
       t.primary_key :id
-    }
-    table(:dogs) { |t|
+    end
+    table(:dogs) do |t|
       t.primary_key :id
       t.integer :handler_id
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :remove_column, table: "dogs",     name: "handler_id" },
         { call: :add_column,    table: "handlers", name: "dog_id", type: :integer }
-      ]}
-    )
+      ]} )
   end
 
   it "adds a polymorphic association" do
-    model(:comments) {
+    model(:comments) do
       fields do |t|
         t.text :content
       end
       belongs_to :commentable, polymorphic: true
-    }
+    end
 
-    table(:comments) { |t|
+    table(:comments) do |t|
       t.primary_key :id
       t.text :content
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :add_column, table: "comments", name: "commentable_id", type: :integer},
         { call: :add_column, table: "comments", name: "commentable_type", type: :string }
-      ]}
-    )
+      ]} )
   end
 
   it "removes a polymorphic association" do
-    model(:comments) {
+    model(:comments) do
       fields do |t|
         t.text :content
       end
-    }
+    end
 
-    table(:comments) { |t|
+    table(:comments) do |t|
       t.primary_key :id
       t.text :content
       t.integer :commentable_id
       t.string :commentable_type
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :remove_column, table: "comments", name: "commentable_id" },
         { call: :remove_column, table: "comments", name: "commentable_type" }
-      ]}
-    )
+      ]} )
   end
 
-  it "adds a habtm table" do
-    model(:users) {
+  it "adds a habtm table when just assoc is changed" do
+    model(:users) do
       fields do |t|
         t.string :name
       end
       has_and_belongs_to_many :roles, foreign_key: 'user_id', association_foreign_key: 'role_id', join_table: 'roles_users'
-    }
+    end
 
-    model(:roles) {
+    model(:roles) do
       fields
       has_and_belongs_to_many :users, foreign_key: 'role_id', association_foreign_key: 'user_id', join_table: 'roles_users'
-    }
+    end
 
-    table(:users) { |t|
+    table(:users) do |t|
       t.primary_key :id
       t.string :name
-    }
+    end
 
-    table(:roles) { |t|
+    table(:roles) do |t|
       t.primary_key :id
-    }
+    end
 
-    test_migrator(
-      { create: [
+    test_migrator({
+      create: [
         { table_name: "roles_users", primary_key: false, columns: [
           { name: "role_id", type: :integer },
           { name: "user_id", type: :integer }
         ]}
-      ]}
-    )
+      ]} )
   end
 
-  it "removes a habtm table" do
-    model(:users) {
+  it "removes a habtm table when just assoc is changed" do
+    model(:users) do
       fields do |t|
         t.string :name
       end
-    }
+    end
 
-    model(:roles) {
+    model(:roles) do
       fields
-    }
+    end
 
-    table(:users) { |t|
+    table(:users) do |t|
       t.primary_key :id
       t.string :name
-    }
+    end
 
-    table(:roles) { |t|
+    table(:roles) do |t|
       t.primary_key :id
-    }
+    end
 
-    table(:roles_users) { |t|
+    table(:roles_users) do |t|
       t.integer :role_id
       t.integer :user_id
-    }
+    end
 
-    test_migrator(
-      { drop: [ "roles_users" ]}
-    )
+    test_migrator({
+      drop: [ "roles_users" ]
+    })
   end
 
   it "changes a column from an integer to a string" do
-    model(:changing_table) {
+    model(:changing_table) do
       fields :id => false do |t|
         t.string :year
       end
-    }
+    end
 
-    table(:changing_table) { |t|
+    table(:changing_table) do |t|
       t.integer :year
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :change_column, table: "changing_table", name: "year", type: :string }
-      ]}
-    )
+      ]} )
   end
 
   it "changes a column that's now :null => false" do
-    model(:non_null) {
+    model(:non_null) do
       fields :id => false do |t|
         t.string :name, :null => false
       end
-    }
+    end
 
-    table(:non_null) { |t|
+    table(:non_null) do |t|
       t.string :name
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :change_column, table: "non_null", name: "name", type: :string, options: { null: false }}
-      ]}
-    )
+      ]} )
   end
 
   it "changes a column that's no longer :null => false" do
-    model(:nullable) {
+    model(:nullable) do
       fields :id => false do |t|
         t.string :name  # null is default
       end
-    }
+    end
 
-    table(:nullable) { |t|
+    table(:nullable) do |t|
       t.string :name, :null => false
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :change_column, table: "nullable", name: "name", type: :string }
-      ]}
-    )
+      ]} )
   end
 
   it "ignores a model column that's :null => true" do
-    model(:nullable) {
+    model(:nullable) do
       fields :id => false do |t|
         t.string :name, null: true
       end
-    }
+    end
 
-    table(:nullable) { |t|
+    table(:nullable) do |t|
       t.string :name
-    }
+    end
 
     test_migrator({})
   end
 
   it "ignores a table column that's :null => true" do
-    model(:nullable) {
+    model(:nullable) do
       fields :id => false do |t|
         t.string :name
       end
-    }
+    end
 
-    table(:nullable) { |t|
+    table(:nullable) do |t|
       t.string :name, null: true
-    }
+    end
 
     test_migrator({})
   end
@@ -256,42 +248,40 @@ describe Richfield::Migrator do
   end
 
   it "removes a table's primary key" do
-    model(:now_no_key) {
+    model(:now_no_key) do
       fields id: false do |t|
         t.string :name
       end
-    }
+    end
 
-    table(:now_no_key) { |t|
+    table(:now_no_key) do |t|
       t.primary_key :id
       t.string :name
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :remove_column, table: "now_no_key", name: "id" }
-      ]}
-    )
+      ]} )
   end
 
   it "changes a table's primary key" do
-    model(:alterkey) {
+    model(:alterkey) do
       fields primary_key: :t2 do |t|
         t.integer :t1, :t2
       end
-    }
+    end
 
-    table(:alterkey) { |t|
+    table(:alterkey) do |t|
       t.primary_key :t1
       t.integer :t2
-    }
+    end
 
-    test_migrator(
-      { change: [
+    test_migrator({
+      change: [
         { call: :change_column, table: "alterkey", name: "t2", type: :primary_key},
         { call: :change_column, table: "alterkey", name: "t1", type: :integer}
-      ]}
-    )
+      ]} )
   end
 
   it "renames columns where possible"
